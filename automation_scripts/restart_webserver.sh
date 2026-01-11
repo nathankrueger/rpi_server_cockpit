b0c2e95188885ef3882be3ca40e2d5ca328e19dd
Nathan Krueger <natekrueger805@gmail.com>
2026-01-11 07:47:39 -0700

    Make the server able to update itself via git pull

--- automation_scripts/restart_webserver.sh
+++ automation_scripts/restart_webserver.sh
@@ -1,1 +1,33 @@
+#!/bin/bash
+
+# Parse options using getopt
+OPTIONS=$(getopt -o "" -l "update" -- "$@")
+if [ $? -ne 0 ]; then
+    echo "Usage: $0 [--update]"
+    exit 1
+fi
+
+eval set -- "$OPTIONS"
+
+UPDATE=false
+
+while true; do
+    case "$1" in
+        --update)
+            UPDATE=true
+            shift
+            ;;
+        --)
+            shift
+            break
+            ;;
+    esac
+done
+
+# Run git pull if --update flag is set
+if [ "$UPDATE" = true ]; then
+    echo "Cloning latest server ..."
+    git pull
+fi
+
 sudo systemctl restart pi-dashboard
